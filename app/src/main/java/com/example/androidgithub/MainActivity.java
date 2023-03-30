3da8249935ab72710447365938c96c70f51d8d9a
kjhoon1410 <kjhoon1410@naver.com>
2023-03-30 13:31:28 +0900

    third commit -- 컴퓨터2 변경

--- app/src/main/java/com/example/androidgithub/MainActivity.java
+++ app/src/main/java/com/example/androidgithub/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //comit1 -- 기본 기능 구현
+    //commit1 -- 기본 기능 구현
     //commit2 -- 기능1 추가
+    //commit3 --컴퓨터2 변경
 }
